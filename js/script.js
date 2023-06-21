f4a9130131ed60189d47bbc604bfe9d5f4db8cb1
auwtorunTest <github.auwtorun@gmail.com>
2023-06-21 09:04:18 +0700

    Update 1.5.0 - Updated a feature to add priority select

--- js/script.js
+++ js/script.js
@@ -9,7 +9,24 @@ window.addEventListener("load", function () {
 });
 
 const taskInput = document.getElementById("taskInput");
+const prioritySelect = document.getElementById("prioritySelect");
 const taskList = document.getElementById("taskList");
+
+function addTask() {
+    const taskText = taskInput.value;
+    const priorityValue = prioritySelect.value;
+  
+    if (taskText !== "") {
+      tasks.push({
+        text: taskText,
+        priority: priorityValue,
+        completed: false,
+      });
+      taskInput.value = "";
+      saveTasksToLocalStorage();
+      renderTasks();
+    }
+  }
 
 taskInput.addEventListener("keypress", function (event) {
     if (event.key === "Enter") {
@@ -17,53 +34,46 @@ taskInput.addEventListener("keypress", function (event) {
     }
   });
 
-function addTask() {
-  const taskText = taskInput.value;
-
-  if (taskText !== "") {
-    tasks.push({
-      text: taskText,
-      completed: false
-    });
-    taskInput.value = "";
-    saveTasksToLocalStorage();
-    renderTasks();
-  }
-}
 
 function renderTasks() {
-    taskList.innerHTML = "";
+  taskList.innerHTML = "";
 
-    tasks.forEach(function (task, index) {
-        const li = document.createElement("li");
-        const taskText = document.createElement("span");
-        const deleteButton = document.createElement("button");
-        const editButton = document.createElement("button");
-        const statusButton = document.createElement("button");
+  tasks.forEach(function (task, index) {
+    const li = document.createElement("li");
+    const taskText = document.createElement("span");
+    const taskPriority = document.createElement("span");
+    const deleteButton = document.createElement("button");
+    const editButton = document.createElement("button");
+    const statusButton = document.createElement("button");
 
-        taskText.textContent = task.text;
-        li.appendChild(taskText);
+    taskText.textContent = task.text;
+    li.appendChild(taskText);
 
-        deleteButton.textContent = "Hapus";
-        deleteButton.addEventListener("click", function () {
-            deleteTask(index);
-        });
-        li.appendChild(deleteButton);
+    taskPriority.textContent = getPriorityLabel(task.priority);
+    taskPriority.classList.add("task-priority-" + task.priority); // Menambahkan kelas CSS sesuai dengan prioritas
+    li.appendChild(taskPriority);
 
-        editButton.textContent = "Edit";
-        editButton.addEventListener("click", function () {
-            editTask(index);
-        });
-        li.appendChild(editButton);
 
-        statusButton.textContent = task.completed ? "Belum Selesai" : "Selesai";
-        statusButton.addEventListener("click", function () {
-            toggleStatus(index);
-        });
-        li.appendChild(statusButton);
-
-        taskList.appendChild(li);
+    deleteButton.textContent = "Hapus";
+    deleteButton.addEventListener("click", function () {
+      deleteTask(index);
     });
+    li.appendChild(deleteButton);
+
+    editButton.textContent = "Edit";
+    editButton.addEventListener("click", function () {
+      editTask(index);
+    });
+    li.appendChild(editButton);
+
+    statusButton.textContent = task.completed ? "Belum Selesai" : "Selesai";
+    statusButton.addEventListener("click", function () {
+      toggleStatus(index);
+    });
+    li.appendChild(statusButton);
+
+    taskList.appendChild(li);
+  });
 }
 
 function deleteTask(index) {
@@ -73,21 +83,33 @@ function deleteTask(index) {
 }
 
 function editTask(index) {
-    const newText = prompt("Masukkan teks baru:", tasks[index].text);
+  const newText = prompt("Masukkan teks baru:", tasks[index].text);
 
-    if (newText !== null && newText !== "") {
-        tasks[index].text = newText;
-        saveTasksToLocalStorage();
-        renderTasks();
-    }
+  if (newText !== null && newText !== "") {
+    tasks[index].text = newText;
+    saveTasksToLocalStorage();
+    renderTasks();
+  }
 }
 
 function toggleStatus(index) {
-    tasks[index].completed = !tasks[index].completed;
-    saveTasksToLocalStorage();
-    renderTasks();
+  tasks[index].completed = !tasks[index].completed;
+  saveTasksToLocalStorage();
+  renderTasks();
 }
 
 function saveTasksToLocalStorage() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+function getPriorityLabel(priorityValue) {
+  if (priorityValue === "green") {
+    return "";
+  } else if (priorityValue === "yellow") {
+    return "";
+  } else if (priorityValue === "red") {
+    return "";
+  }
+
+  return "";
 }
